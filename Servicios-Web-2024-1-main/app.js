138dddfd261b4ae17cdbc1363e0e0a927b2bd6b1
Dark Doctor <145211989+CamiloH96@users.noreply.github.com>
2024-02-28 20:58:56 -0500

    Add files via upload

---
+++ Servicios-Web-2024-1-main/app.js
@@ -0,0 +1,36 @@
+/*
+console.log("Hello CIAF 2024 - 1");
+
+console.error("Esto es un error");
+
+console.warn("Este mensaje es de advertencia");
+
+const persona=[
+    {name:"Camilo",lastName:"Hernandez",phone:321776}
+]
+
+*/
+/*
+//Forma de variables
+
+var firsName= "Camilo" //Variable = Scope super global
+let lastName= "Hernandez" //Variable = Scope local
+const age = 27 //Valor no cambia = Scope global
+
+
+
+function printFullName(){
+    let lastName = "Ibarguen"; //Aqui solo imprime el apellido
+}
+
+printFullName();
+console.log(lastName);
+*/
+
+//Concatenar datos
+//Forma 1:(+)
+console.log(firsName+lastName);
+
+//Forma 2:
+const fullNameTwo= `Este es el nombre completo : ${firsName} ${lastName}`;
+console.log(fullNameTwo);
