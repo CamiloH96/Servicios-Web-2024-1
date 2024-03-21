784154cf847f3f961b8246939386354f4c3bab8b
Camilo <camilohi.1196@gmail.com>
2024-03-20 20:50:51 -0500

    Prueba

--- Temas/operadores-logicos.js
+++ Temas/operadores-logicos.js
@@ -2,7 +2,15 @@ console.log("<==================================================================
 console.log("Operadores logicos");
 
 console.log("Operadores AND &&");
+console.log(false && false); //false
+console.log(true && false); //false
+console.log(false && true); //false
+console.log(true && true); //true
 
 console.log("Operadores OR ||");
+console.log(false || false); //true
+console.log(true || false); //true
+console.log(false || true); //true
+console.log(true || true); //true
 
 console.log("Operadores NOT !");
