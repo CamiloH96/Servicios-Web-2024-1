22e4ccdc60afbbba2426eb7ea0b751d3b24347bf
Camilo <camilohi.1196@gmail.com>
2024-03-20 20:40:10 -0500

    Clase 20/03/2024

---
+++ Temas/operadores-logicos.js
@@ -0,0 +1,8 @@
+console.log("<=========================================================================>");
+console.log("Operadores logicos");
+
+console.log("Operadores AND &&");
+
+console.log("Operadores OR ||");
+
+console.log("Operadores NOT !");
