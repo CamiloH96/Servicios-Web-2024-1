ba37d48d7f217f79c4e0cf0fd050d109f3942ff9
Camilo <camilohi.1196@gmail.com>
2024-03-20 20:57:45 -0500

    Prueba

--- Temas/operadores-logicos.js
+++ Temas/operadores-logicos.js
@@ -14,3 +14,14 @@ console.log(false || true); //true
 console.log(true || true); //true
 
 console.log("Operadores NOT !");
+
+let counter = 10;
+console.log(`el valor del contador es ${counter}`);
+
+if (counter > 100 ){
+    console.log(`el valor de counter: ${counter} es mayor a 100`);
+}else{
+    console.log(`el valor del counter: ${counter} es menor a 100`)
+}
+
+counter > 100 ? console.log("counter > 100"): console.log("counter < 100");
